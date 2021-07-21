43a362adc0c0bc35196ac9c18fa5edf73bdeb749
MaikHo <maikhoffmann@outlook.com>
2021-07-21 09:03:59 +0200

    inner class und nested class

--- src/oop/InnerClasses.kt
+++ src/oop/InnerClasses.kt
@@ -1,2 +1,23 @@
 package oop
 
+class Outer{
+    val name = "Hans"
+    val fromNested = Outer.Nested().foo()
+
+    class Nested{
+
+        fun foo() = "Hallo"  // kann nicht auf die Member der Outer class zugreifen
+    }
+
+    inner class Inner{
+        val outer = "name"
+        fun bar() = "Hallo $name"
+    }
+}
+
+fun main(){
+    val demo = Outer.Nested().foo()
+    val demo2 = Outer().fromNested
+
+    val bar = Outer().Inner().bar()
+}
