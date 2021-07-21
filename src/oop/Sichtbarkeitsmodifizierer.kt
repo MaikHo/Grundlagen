fd2612a7543c1193332df64cbb42eb883c82f8da
MaikHo <maikhoffmann@outlook.com>
2021-07-21 09:17:24 +0200

    Datenkapslung

---
+++ src/oop/Sichtbarkeitsmodifizierer.kt
@@ -0,0 +1,23 @@
+package oop
+
+/*
+Betrifft Datenkapslung
+
+public
+- Überall sichtbar
+
+protect
+- nur in Unterklassen (Vererbung) sichtbar
+
+internal
+- Vergleichbar mit default von Java
+- nur im selben Modul sichtbar
+
+private
+- nicht von außen sichtbar
+
+
+
+
+
+ */
