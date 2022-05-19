d4c975802de37b7cbfa7ba8aa411f5d526c7b435
Maik Hoffmann <maikhoffmann@outlook.com>
2022-05-19 11:11:33 +0200

    Create DataClassBewerbung.kt

---
+++ src/first/DataClassBewerbung.kt
@@ -0,0 +1,5 @@
+data class Bewerbung(var company: String? = "visoma",
+                     var street: String? = "Schillerstraße",
+                     var streetnumber: String? = "1",
+                     var zip: Int? = 29525,
+                     var city: String? = "Uelzen")
